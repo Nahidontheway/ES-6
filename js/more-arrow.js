e3cbd755eb8ced81b5c6de691015410018a93a72
Nahidontheway <100711410+Nahidontheway@users.noreply.github.com>
2022-08-26 22:51:51 +0600

    more arrow function

---
+++ js/more-arrow.js
@@ -0,0 +1,20 @@
+ const addition = (a,b) => a+b;
+ const fullName = (first,second) => first + ' '+ second;
+ const multiply = (a,b) => a*b;
+
+ const result = addition(5,3);
+ console.log(result);
+ const final = multiply(3,6);
+ console.log(final);
+ const fullNameDec = fullName('Nahid','Hasan');
+ console.log(fullNameDec);
+
+ const multipleOperation = (a,b,c) =>{
+    const x = a+b;
+    const y = b+c;
+    const z = x/y;
+    return z;
+ }
+ const finalResult = multipleOperation(2,3,5);
+ console.log(finalResult);
+  
