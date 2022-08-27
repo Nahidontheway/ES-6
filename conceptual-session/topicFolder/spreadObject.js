2fcca67e9475b87a3f34991d65a7b3e9485a5a78
Nahidontheway <100711410+Nahidontheway@users.noreply.github.com>
2022-08-27 14:26:03 +0600

    object destruct in js

---
+++ conceptual-session/topicFolder/spreadObject.js
@@ -0,0 +1,18 @@
+const Person = {
+    name : 'nahid',
+    age : 22
+    
+}
+
+const user = {
+    phone : '235632',
+    address : 'bari nai',
+    p : [2,5,6,8,9]
+} 
+const {p:number} = user;
+
+for(let i of number){
+    console.log(i);
+}
+
+
