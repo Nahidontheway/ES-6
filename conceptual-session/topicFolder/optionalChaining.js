aaf51fbbef13ebc047db7489e42f13f26e4a0bf8
Nahidontheway <100711410+Nahidontheway@users.noreply.github.com>
2022-08-27 16:45:38 +0600

    optional chaininig in js

---
+++ conceptual-session/topicFolder/optionalChaining.js
@@ -0,0 +1,17 @@
+const person = {
+    name : 'adnan adie',
+    address : {
+        location : '2/A street',
+        home : 'pallabi, dhamondi'
+    },
+    pet : {
+        name : 'dog',
+        age : 5,
+        homeAdress : {
+            home : 'Australia',
+            price : '2000$'
+        }
+    }
+}
+console.log(person.home?.address);
+console.log('hello')
