00e004e631b06b98ea81382beee1601e513428f5
Nahidontheway <100711410+Nahidontheway@users.noreply.github.com>
2022-08-27 12:53:58 +0600

    let const

---
+++ conceptual-session/topicFolder/letConst.js
@@ -0,0 +1,16 @@
+var Name = 'Nahid';
+Name = 'Hasan'; // we can re-assign var
+var name = 'Tanjim'; // we can re-define var
+console.log(Name);
+
+
+let Name = 'Nahid';
+Name = 'Hasan'; // we can re-assign var
+let name = 'Tanjim'; // we can re-define var
+console.log(name);
+
+
+const Name = 'Nahid'
+Name = 'Hasan'; // we can not re-assign var
+const name = 'Tanjim'; // we can not re-define var
+console.log(Name);
